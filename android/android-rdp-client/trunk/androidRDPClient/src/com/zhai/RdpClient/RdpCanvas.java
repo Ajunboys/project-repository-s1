0c9d087b8582c7279f489b780fd5254d8b9a4b22
Ajunboys <weijunqiang2010@foxmail.com>
2017-07-17 15:22:52 +0800

    帅帅哒
    ####Android rdp client

---
+++ android/android-rdp-client/trunk/androidRDPClient/src/com/zhai/RdpClient/RdpCanvas.java
@@ -0,0 +1,31 @@
+package com.zhai.RdpClient;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class RdpCanvas extends ImageView {
+
+	public RdpCanvas(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RdpCanvas(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RdpCanvas(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		// TODO Auto-generated method stub
+		super.onDraw(canvas);
+	}
+
+}
