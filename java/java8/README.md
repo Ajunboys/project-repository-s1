cbd886cc925ec206617630b45dca6f2d8ebcccd8
Ajunboys <weijunqiang2010@foxmail.com>
2017-07-28 17:25:08 +0800

    Java8 新特性探究

---
+++ java/java8/README.md
@@ -0,0 +1,2 @@
+#Java8 新特性探究
+### [可访问在线文档](https://wizardforcel.gitbooks.io/java8-new-features/content/1.html\) - [2](https://wizardforcel.gitbooks.io/java8-new-features/content/\) 
--- java/java8/src/main/java/git/ajunboys/project/java8/example/HelloWorld.java
+++ java/java8/src/main/java/git/ajunboys/project/java8/example/HelloWorld.java
@@ -1,13 +1,13 @@
 package git.ajunboys.project.java8.example;
 
 public class HelloWorld {
-	@SuppressWarnings("static-access")
-	public static void main(String[] args) {
+	
+	public static void runThreadUseInnerClass(){
 		Runnable runnable = () -> {
 			int i = 0;
 			while (i < 10) {
 				
-				System.out.println("["+System.currentTimeMillis() + "]:Hello world");
+				System.out.println("Inner Class:["+System.currentTimeMillis() + "]:Hello world");
 				
 				i++;
 				
@@ -22,5 +22,32 @@ public class HelloWorld {
 		};
 		
 		new Thread(runnable).start();
+	}
+	
+	public static void runThreadUseLambda(){
+		new Thread(() -> {
+			int i = 0;
+			while (i < 10) {
+				
+				System.out.println("Lambda Class:["+System.currentTimeMillis() + "]:Hello world");
+				
+				i++;
+				
+				try {
+					Thread.currentThread().sleep(1000L);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+		}).start();
+	}
+	
+	@SuppressWarnings("static-access")
+	public static void main(String[] args) {
+		HelloWorld.runThreadUseInnerClass();
+		
+		HelloWorld.runThreadUseLambda();
 	}
 }
